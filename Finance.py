78de64881575d2aad16953647edb8c910a3a6385
Praise-Zee <ifeanyiozioma2019@gmail.com>
2024-10-11 18:17:21 +0100

    After working on the finance.py file i have to update on github

--- Finance.py
+++ Finance.py
@@ -1,5 +1,10 @@
 import math
-income = (int(input("enter your income")))
-expenses = (int(input("enter your expenses")))
-netSavings = income - expenses
-print(netSavings)
+income = int(input("Enter your monthly income: ₦ "))
+incomeDescription = input("Enter income description")
+expense1 = int(input("Enter expense amount: ₦"))
+expenseDescription1 = input("Enter expense description")
+expense2 = int(input("Enter 2nd expense amount: ₦"))
+expenseDescription2 = input("Enter 2nd expense description")
+Totalexpense = expense1 + expense2
+netSavings = income - Totalexpense
+print("income: ₦",income, "Totalexpense: ₦", Totalexpense, "netSavings: ₦", netSavings)
