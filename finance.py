a5686d216c615507b775a4828ea83036a35543e3
Praise-Zee <ifeanyiozioma2019@gmail.com>
2024-10-15 19:59:01 +0100

    pushing after adding names of files

--- finance.py
+++
@@ -1,9 +0,0 @@
-incomeAmount = int(input("Enter your Monthly income"))
-incomeDescription = input("Enter income description")
-expenseAmount = int(input("Enter expenses"))
-expenseDescription = input("Enter expense description")
-2expenseAmount = int(input("Enter 2nd expense"))
-2expenseDescription = input("Enter 2nd expenxe description")
-Totalexpense = expenseAmount + 2expenseAmount
-netSaving = incomeAmount - Totalexpense
-print("Your income is" + incomeAmount "while your expense is" + Totalexpense "therefore your balance is" + netSaving)
